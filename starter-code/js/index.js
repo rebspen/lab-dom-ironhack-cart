4b5765eb0393bcabb1bd05bd669cf756368a88d4
Rebecca Spence <Bec@Rebeccas-MacBook-Pro.local>
2019-10-28 15:48:44 +0000

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,14 +1,29 @@
 // ITERATION 1
 
 function updateSubtotal($product) {
-  // ...
-}
+  const $price = $product.querySelector('.price span').innerText;
+  const $quantity = $product.querySelector('.quantity input').valueAsNumber;
+  const $subtotal = $product.querySelector('.subtotal');
+
+  return $subtotal.querySelector('span').innerText = $price * $quantity
+
+};
+
 
 function calculateAll() {
-  // ITERATION 2
-  //...
-  // ITERATION 3
-  //...
+  const $product = document.querySelectorAll('.product');
+[...$product].forEach(product => {
+  updateSubtotal(product);
+});
+
+let total = 0
+const $totalPrice = document.querySelector('h2 span');
+console.log($product);
+[...$product].forEach(product => {
+  total +=  parseFloat(product.querySelector('.subtotal span').innerHTML);
+});
+
+return $totalPrice.innerHTML =  total
 }
 
 const $calculateTrigger = document.getElementById('calculate');
